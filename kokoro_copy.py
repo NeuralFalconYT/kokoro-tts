60cc2a0da0e8d0c97d33f6a0f9559c7aec170384
Neural Falcon <neuralfalconofficial@gmail.com>
2024-12-31 14:42:39 +0530

    Update kokoro_copy.py

--- kokoro_copy.py
+++ kokoro_copy.py
@@ -10,6 +10,7 @@ if os.path.exists(temp_folder):
         pass
 os.makedirs(temp_folder, exist_ok=True)
 
+#copied from https://huggingface.co/spaces/hexgrad/Kokoro-TTS/blob/main/app.py
 
 import phonemizer
 import re
