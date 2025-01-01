cbd3b172fba47d03238f96676795cae9aa4eadee
Neural Falcon <neuralfalconofficial@gmail.com>
2025-01-01 18:37:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -112,5 +112,26 @@ import click
 @click.option("--share", is_flag=True, default=False, help="Enable sharing of the interface.")
 def main(debug, share):
     demo.queue().launch(debug=debug, share=share)
+    # demo.queue().launch(debug=debug, share=share,server_port=8081)
 if __name__ == "__main__":
     main()    
+
+#Use in Other Projects
+# from gradio_client import Client
+# import shutil
+# import os
+# os.makedirs("temp_audio", exist_ok=True)
+# client = Client("http://127.0.0.1:8081/")
+# result = client.predict(
+# 		text="Hello!!",
+# 		voice_name="af_bella",
+# 		speed=1,
+# 		trim=0,
+# 		pad_between_segments=0,
+# 		remove_silence=False,
+# 		minimum_silence=0.05,
+# 		api_name="/text_to_speech"
+# )
+# save_at=f"./temp_audio/{os.path.basename(result)}"
+# shutil.move(result, save_at)
+# print(f"Saved at {save_at}")
